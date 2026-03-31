da4f29d4e270e21b16e6889469f2f0b6ae016b2a
Alex <alessandro.piccione.75@gmail.com>
2026-03-31 09:54:49 +0200

    add F# project for core library

---
+++ src/ClassLibrary1/Domain.fs
@@ -0,0 +1,42 @@
+﻿namespace ClassLIbrary1.Core
+
+/// Represents a person in the family tree
+type Person = {
+    Id: string
+    FirstName: string
+    LastName: string
+    BirthDate: System.DateTime option
+    DeathDate: System.DateTime option
+    Gender: Gender
+    Biography: string option
+}
+
+/// Gender enumeration
+and Gender = 
+    | Male
+    | Female
+    | Unknown
+
+/// Represents a relationship between two people
+type Relationship = {
+    Person1Id: string
+    Person2Id: string
+    RelationshipType: RelationshipType
+}
+
+/// Types of relationships
+and RelationshipType =
+    | ParentChild
+    | Spouse
+    | Sibling
+
+/// The main family tree structure
+type FamilyTree = {
+    Id: string
+    Name: string
+    Description: string option
+    People: Map<string, Person>
+    Relationships: Relationship list
+    CreatedDate: System.DateTime
+    ModifiedDate: System.DateTime
+}
---
+++ src/MauiApp1/GlobalUsings.cs
@@ -0,0 +1,1 @@
+﻿global using MauiApp1.Pages;
